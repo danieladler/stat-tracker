b28d14a0e4e296da657cc4994c2c0a04361183ed
Daniel Adler <weston.adler@gmail.com>
2015-11-25 16:53:10 -0800

    added JS to improve table formatting

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -17,4 +17,34 @@ $(document).ready(function() {
     $(".submit-form-container").toggle();
   });
 
+  // SANDBOX: scroll to error messages if present
+  // if ($(".error-list").length == 0) {
+  //   $('html, body').animate({
+  //        scrollTop: ($('.error-list').offset().top - 300)
+  //   }, 2000);
+
+
+  // if ($(".error-list").length == 0) {
+  //    $('html, body').animate({
+  //
+  //     })
+  //   } else {
+  //   $('html, body').animate({
+  //        scrollTop: ($('.error-list').offset().top - 300)
+  //   }, 2000);
+  // }
+
+  // alternating format on table rows
+  $("#table tbody tr:even").css("background-color", 'rgba(0, 0, 0, 0.15)');
+
+  // remove repeated dates
+  var seen = {};
+  $('table tbody tr .date-row').each(function() {
+    var txt = $(this).text();
+    if (seen[txt])
+        $(this).text("");
+    else
+        seen[txt] = true;
+  });
+
 });
